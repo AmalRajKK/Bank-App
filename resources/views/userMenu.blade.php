485a9116a0173c7ddab12b991524fb11de6610dc
itsamal <amalrajkk2000@gmail.com>
2024-08-07 14:33:36 +0530

    third commit

--- resources/views/userMenu.blade.php
+++ resources/views/userMenu.blade.php
@@ -1,7 +1,19 @@
 <x-layout>
     <x-slot:title>User Menu</x-slot:title>
     <x-slot:heading>User Menu</x-slot:heading>
-    <h1>WELCOME:{{Auth::user()->name }}</h1>
-    <h1>USER Id:{{ Auth::user()->email}}</h1>
-    <h1>BALANCE:{{ $account->balance}}</h1>
+    <div style="padding: 25px">
+    <h1>WELCOME  : {{ Auth::user()->name }} </h1>
+    </div>
+    <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="py-2 px-4 text-left">Your Id</th>
+                <th class="py-2 px-4 text-left">Your Balance</th>
+            </tr>
+        </thead>
+        <tbody>
+                <td class="py-2 px-4">{{ Auth::user()->email}}</td>
+                <td class="py-2 px-4">{{ $account->balance }}</td>
+        </tbody>
+    </table>
 </x-layout>
